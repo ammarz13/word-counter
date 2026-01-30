454e72963439dab3538497f0ed176b03c4daeff7
Ammar <apple@192.168.10.6>
2026-01-30 21:38:43 +0500

    dark mode add on thi sapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import { useState } from 'react';
+import Header from './components/Header';
 import Uppercase from './components/UpperCase';
 
 function App() {
+  const [mode , setMode] = useState('light');
+
+
+  const modeChnage = ()=>{
+    if(mode === 'dark'){
+      setMode('light');
+      document.body.style.backgroundColor = '#ffffff';
+      
+    } else {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+   
+  }
   return (
     <>
-    <Uppercase title="Text Counter"/>
+    <Header title="WordCounter" mode={mode} toogleMode={modeChnage}/>
+    <Uppercase title="Text Counter" mode={mode}/>
     </>
   );
 }
--- src/components/UpperCase.jsx
+++ src/components/UpperCase.jsx
@@ -1,9 +1,12 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types';
 
+
 function UpperCase(props) {
 
 const [text, setText] = useState("");
+
+
 
 
 const handleUpper = () =>{
@@ -14,6 +17,7 @@ const handleUpper = () =>{
 
 const handleChange = (event) =>{
     //console.log(setText)
+    
     setText(event.target.value);
    
    
@@ -30,22 +34,70 @@ const handleClear = () =>{
     setText(newText);
 }
 
+
+const copyText = (e) =>{
+  // const copyWord = text.select();
+  // setText(copyWord);
+  if (text === ""){
+    setText("Nothing to copy");
+  } else {
+    
+  }
+
+  
+  navigator.clipboard.writeText(e.target.text);
+}
+
+
+// const toggleChnage = () =>{
+
+//   if(mystyle.color === 'white'){
+//     setMyStyle({
+//       color: 'black',
+//       backgroundColor: 'white'
+//     })
+//     setTextUpdate("Light Mode");
+//     document.body.classList.add('light-mode');
+//     document.body.classList.remove('dark-mode');
+
+//   } else {
+//     setMyStyle({
+//       color: 'white',
+//       backgroundColor: 'black'
+//     })
+//     setTextUpdate("Dark Mode");
+//     document.body.classList.add('dark-mode');
+//     document.body.classList.remove('light-mode');
+//   }
+  
+//   setMyStyle({
+//     color: mystyle.color === 'black' ? 'white' : 'black',
+//     backgroundColor: mystyle.backgroundColor === 'white' ? 'black' : 'white',
+   
+//   })
+//   setTextUpdate("Dark Mode");
+// }
+
   return (
-    <div>
+    <div style={{color: props.mode === 'dark' ? 'white' : 'black' }}>
         
-      <div className='container mt-5'>
+      <div className="container mt-5">
         <div className="mb-3">
         <h1>{props.title}</h1>
+        
          
-          <textarea type="text"   className="form-control my-3" rows="8" value={text} onChange={handleChange} />
+          <textarea type="text"   className="form-control my-3" rows="8" value={text} onChange={handleChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}/>
 
          <div className='d-flex gap-2'>
-         <button className='mt-2 btn btn-primary' onClick={handleUpper}>UpperCase</button>
+         <button className='mt-2 btn btn-primary' onClick={handleUpper} disabled={!text.trim()}>UpperCase</button>
 
-<button className='mt-2 btn btn-primary' onClick={handleLower}>LowerCase</button>
+<button className='mt-2 btn btn-primary' onClick={handleLower} disabled={!text.trim()}>LowerCase</button>
+<button className='mt-2 btn btn-primary' onClick={copyText} disabled={!text.trim()}>CopyText</button>
 
 
-<button className='mt-2 btn btn-danger' onClick={handleClear}>Clear All</button>
+<button className={`mt-2 btn btn-danger`} onClick={handleClear} disabled={!text.trim()}>Clear All</button>
+{/* <button className='mt-2 btn btn-primary' onClick={toggleChnage}>{textUpdate}</button> */}
+
          </div>
         </div>
       </div> 
@@ -58,7 +110,7 @@ const handleClear = () =>{
 
 
           <h2>Preview</h2>
-          <p>{text}</p>
+          <p>{text.length>0 ? text : 'Enter text in above textarea to preview'}</p>
         </div>
       </div> 
     </div>
